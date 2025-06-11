593adced9c32b80e4afc6cc52dbc9d9992dd54a4
idrimi <gerber@byrds.ch>
2025-06-11 08:31:38 +0200

    add capacitor and compile iOS POC

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ch.byrds.angularsnapcast',
+  appName: 'angular-snapcast',
+  webDir: 'www'
+};
+
+export default config;
