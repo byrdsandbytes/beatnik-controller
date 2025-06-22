d6e2d5e86767a8a67221baaa58cfaa682e5559b2
idrimi <gerber@byrds.ch>
2025-06-22 08:39:04 +0200

    add devices page

--- src/app/pages/devices/devices.page.ts
+++ src/app/pages/devices/devices.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SnapCastServerStatusResponse } from 'src/app/model/snapcast.model';
+import { SnapcastService } from 'src/app/services/snapcast.service';
 
 @Component({
   selector: 'app-devices',
@@ -8,9 +11,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevicesPage implements OnInit {
 
-  constructor() { }
+  serverState?: Observable<SnapCastServerStatusResponse>;
+
+  constructor(
+    private snapcastService: SnapcastService 
+  ) { }
 
   ngOnInit() {
+    this.serverState = this.snapcastService.state$;
+
+    
+
   }
 
 }
