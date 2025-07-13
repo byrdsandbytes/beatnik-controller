3c3ffa031dfe28d352c98a2fb3c82659a8bbb29e
idrimi <gerber@byrds.ch>
2025-07-13 11:21:36 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,22 +6,30 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
-  {
-    path: 'streams',
-    loadChildren: () => import('./pages/streams/streams.module').then( m => m.StreamsPageModule)
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
-  },
+  // {
+  //   path: 'streams',
+  //   loadChildren: () => import('./pages/streams/streams.module').then( m => m.StreamsPageModule)
+  // },
+  // {
+  //   path: 'dashboard',
+  //   loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  // },
   {
     path: 'server-status',
     loadChildren: () => import('./pages/server-status/server-status.module').then( m => m.ServerStatusPageModule)
   },
-  {
-    path: 'stream-details',
-    loadChildren: () => import('./pages/streams/stream-details/stream-details.module').then( m => m.StreamDetailsPageModule)
-  },
+  // {
+  //   path: 'settings',
+  //   loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
+  // },
+  // {
+  //   path: 'stream-details',
+  //   loadChildren: () => import('./pages/streams/stream-details/stream-details.module').then( m => m.StreamDetailsPageModule)
+  // },
+  // {
+  //   path: 'menu',
+  //   loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
+  // },
  
 ];
 @NgModule({
