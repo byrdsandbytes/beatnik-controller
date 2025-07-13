12252624c80d4fbba0d43a3a72997a3c8716be28
idrimi <gerber@byrds.ch>
2025-07-13 11:21:01 +0200

    routing

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -39,6 +39,11 @@ const routes: Routes = [
           }
         ]
       },
+      {
+        path: 'menu',
+        loadChildren: () => import('../pages/menu/menu.module').then(m => m.MenuPageModule)
+
+      },
 
       {
         path: '',
