a907e6fe69358de2f8f5981ceb3d2408070d7f95
idrimi <gerber@byrds.ch>
2025-10-05 10:32:39 +0200

    camillaDSP Congig model

---
+++ src/app/model/camilla-dsp.model.ts
@@ -0,0 +1,66 @@
+
+
+
+export interface CamillaDspConfig {
+  title: string | null;
+  description: string | null;
+  devices: Devices;
+  mixers: any;
+  filters: { [key: string]: Filter };
+  processors: any;
+  pipeline: Pipeline[];
+}
+
+export interface Devices {
+  samplerate: number;
+  chunksize: number;
+  queuelimit: number | null;
+  silence_threshold: number | null;
+  silence_timeout: number | null;
+  capture: Capture;
+  playback: Playback;
+  enable_rate_adjust: boolean | null;
+  target_level: number | null;
+  adjust_period: number | null;
+  resampler: any;
+  capture_samplerate: number | null;
+  stop_on_rate_change: boolean | null;
+  rate_measure_interval: number | null;
+  volume_ramp_time: number | null;
+}
+
+export interface Capture {
+  type: string;
+  channels: number;
+  device: string;
+  format: string;
+}
+
+export interface Playback {
+  type: string;
+  channels: number;
+  device: string;
+  format: string;
+}
+
+export interface Filter {
+  type: string;
+  description: string | null;
+  parameters: BiquadParameters;
+}
+
+export interface BiquadParameters {
+  type: 'Peaking' | 'Lowshelf' | 'Highshelf';
+  freq: number;
+  q: number;
+  gain: number;
+}
+
+export interface Pipeline {
+  type: string;
+  channel: number;
+  names: string[];
+  description: string | null;
+  bypassed: boolean | null;
+}
+
